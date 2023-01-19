c1d1d19253f668cb3653f443286074482e756b6d
Satch5918 <satch_elfante@hotmail.com>
2023-01-19 13:01:39 -0300

    LISTO

--- src/layouts/navbar/NavBar.jsx
+++ src/layouts/navbar/NavBar.jsx
@@ -14,11 +14,16 @@ const Navbar = () => {
             </div>
             <img className="imgNav" src="/assets/logo1.png" alt="" />
             <div className={`nav_items ${isOpen && "open"}`}>
+            <h2 className={`otracosa ${isOpen && "open"}`}>pepito@petito.com</h2>
             <Anchor className="anchor" to={"/"}>Home</Anchor>
-            <Anchor className="anchor" to={"/comics"}>My comics</Anchor>
-            <Anchor className="anchor" to={"#"}>Logout</Anchor>      
+            <Anchor className="anchor" to={"/comics"}>Comics</Anchor>
+            <Anchor className="anchor" to={"/my-comics"}>My comics</Anchor>
+            <Anchor className="anchor" to={"/new-author"}>New Author</Anchor>
+            <Anchor className="anchor" to={"/new-chapter"}>New Chapter</Anchor>
+            <Anchor className="anchor" to={"/new-cia"}>New Cia.</Anchor>
+            <Anchor className="anchor" to={"/"}>Logout</Anchor>      
             </div>
-            
+
         </div>
         </div>
     )
